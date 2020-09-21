da40f01ff5ea71a399cbcb7a3eaa207c74663bc8
Rishav Nath Pati <patirishavnath@gmail.com>
2020-09-21 16:57:00 +0530

    Registration page complete

--- register.php
+++ register.php
@@ -1,69 +1,97 @@
 <?php
-
 require_once "config.php";
+
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-  //Check if username is empty
+  // Check if username is empty
   if (empty(trim($_POST["username"]))) {
-    $username_err = "Username cannot be empty";
+    $username_err = "Username cannot be blank";
   } else {
-    $sql = "SELECT id FROM users WHERE username=?";
-    $stmt = mysqli_prepare($link, $sql);
+    $sql = "SELECT id FROM users WHERE username = ?";
+    $stmt = mysqli_prepare($conn, $sql);
     if ($stmt) {
       mysqli_stmt_bind_param($stmt, "s", $param_username);
-      $param_username = trim($_POST["username"]);
 
-      //Try execuing this statement
+      // Set the value of param username
+      $param_username = trim($_POST['username']);
+
+      // Try to execute this statement
       if (mysqli_stmt_execute($stmt)) {
         mysqli_stmt_store_result($stmt);
         if (mysqli_stmt_num_rows($stmt) == 1) {
-          $username_err = "This username is already used";
+          $username_err = "This username is already taken";
         } else {
-          $username = trim($_POST["username"]);
+          $username = trim($_POST['username']);
         }
       } else {
         echo "Something went wrong";
       }
     }
   }
+
   mysqli_stmt_close($stmt);
-}
 
-if (empty(trim($_POST['password']))) {
-  $password_err = "Password cannot be empty";
-} elseif (strlen(trim($_POST['password'])) < 5) {
-  $password_err = "Password must be at least 5 characters";
-} else {
-  $password = trim($_POST["password"]);
-}
 
-//Check for confirm_password
-if (empty(trim($_POST['confirm_password']))) {
-  $password_err = "Password cannot be empty";
+  // Check for password
+  if (empty(trim($_POST['password']))) {
+    $password_err = "Password cannot be blank";
+  } elseif (strlen(trim($_POST['password'])) < 5) {
+    $password_err = "Password cannot be less than 5 characters";
+  } else {
+    $password = trim($_POST['password']);
+  }
+
+  // Check for confirm password field
+  if (trim($_POST['password']) !=  trim($_POST['confirm_password'])) {
+    $password_err = "Passwords should match";
+  }
+
+
+  // If there were no errors, go ahead and insert into the database
+  if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $stmt = mysqli_prepare($conn, $sql);
+    if ($stmt) {
+      mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+      // Set these parameters
+      $param_username = $username;
+      $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+      // Try to execute the query
+      if (mysqli_stmt_execute($stmt)) {
+        header("location: login.php");
+      } else {
+        echo "Something went wrong... cannot redirect!";
+      }
+    }
+    mysqli_stmt_close($stmt);
+  }
+  mysqli_close($conn);
 }
 
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
   <!-- Required meta tags -->
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <title>Registration Page</title>
 </head>
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Php Login System</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -73,41 +101,47 @@ if (empty(trim($_POST['confirm_password']))) {
           <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./login.php">Login</a>
+          <a class="nav-link" href="register.php">Register</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact Us</a>
+          <a class="nav-link" href="login.php">Login</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">Logout</a>
+        </li>
+
+
+
       </ul>
     </div>
   </nav>
 
   <div class="container mt-4">
-    <h3>Please register here:</h3>
-    <hr />
-    <form>
+    <h3>Please Register Here:</h3>
+    <hr>
+    <form action="" method="post">
       <div class="form-row">
         <div class="form-group col-md-6">
-          <label for="inputEmail4">Email</label>
-          <input type="email" class="form-control" id="inputEmail4" />
+          <label for="inputEmail4">Username</label>
+          <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Email">
         </div>
         <div class="form-group col-md-6">
           <label for="inputPassword4">Password</label>
-          <input type="password" class="form-control" id="inputPassword4" />
+          <input type="password" class="form-control" name="password" id="inputPassword4" placeholder="Password">
         </div>
       </div>
       <div class="form-group">
-        <label for="inputAddress">Address</label>
-        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+        <label for="inputPassword4">Confirm Password</label>
+        <input type="password" class="form-control" name="confirm_password" id="inputPassword" placeholder="Confirm Password">
       </div>
       <div class="form-group">
         <label for="inputAddress2">Address 2</label>
-        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="inputCity">City</label>
-          <input type="text" class="form-control" id="inputCity" />
+          <input type="text" class="form-control" id="inputCity">
         </div>
         <div class="form-group col-md-4">
           <label for="inputState">State</label>
@@ -118,12 +152,12 @@ if (empty(trim($_POST['confirm_password']))) {
         </div>
         <div class="form-group col-md-2">
           <label for="inputZip">Zip</label>
-          <input type="text" class="form-control" id="inputZip" />
+          <input type="text" class="form-control" id="inputZip">
         </div>
       </div>
       <div class="form-group">
         <div class="form-check">
-          <input class="form-check-input" type="checkbox" id="gridCheck" />
+          <input class="form-check-input" type="checkbox" id="gridCheck">
           <label class="form-check-label" for="gridCheck">
             Check me out
           </label>
@@ -132,11 +166,12 @@ if (empty(trim($_POST['confirm_password']))) {
       <button type="submit" class="btn btn-primary">Sign in</button>
     </form>
   </div>
+
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
